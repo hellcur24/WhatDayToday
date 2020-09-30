649134e56b484a74a7e5c07aeebdfda1620d2507
Opilane TTHK <opilane@tthk.ee>
2020-09-30 09:26:05 +0300

    Wat

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const ejs = require('ejs');
 const app = express();
 
+app.use(express.static('public'));//мы разрешаем испльзовать эту папку, или не будет работать
 app.set('view engine', ejs);
+
 
 app.get('/date',(req,res)=>{
     let today =new Date();
     let day ='';
+    let personData = {
+                firstName:"John",
+                lastName:"Doe",
+                age:34,
+                job:"Deleloper"
+                };
 
     if(today.getDay() === 6 || today.getDate()=== 0){
         day="Weekend!";
@@ -14,7 +22,9 @@ app.get('/date',(req,res)=>{
         day = "Workday";
     }
     
-    res.render('index.ejs', {kindOfDay: day});
+    res.render('index.ejs', {kindOfDay: day,
+                            person:personData
+                            });
 });
 
 app.listen(5000,()=>{
